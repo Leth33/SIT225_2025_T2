de3ad89baf54d122072cb4e861a188fc3b0a3187
Lethe <barbarareincalma@gmail.com>
2025-08-19 20:01:01 +1000

    Update hello_arduino.ino

--- SIT225_2025_T2/week-1/hello_arduino.ino
+++ SIT225_2025_T2/week-1/hello_arduino.ino
@@ -1,22 +1,29 @@
-int ledPin = 13;
+const int LED_PIN = LED_BUILTIN;
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  Serial.setTimeout(2000); // 2s read timeout
 }
 
 void loop() {
-  if (Serial.available()) {
-    int blinkCount = Serial.parseInt();
+  if (Serial.available() > 0) {
+    long n = Serial.parseInt();          
+    if (n <= 0) return;                  
 
-    for (int i = 0; i < blinkCount; i++) {
-      digitalWrite(ledPin, HIGH);
+    // Blink N times for visual confirmation (1/2 s on + 1/2 s off)
+    for (long i = 0; i < n; i++) {
+      digitalWrite(LED_PIN, HIGH);
       delay(500);
-      digitalWrite(ledPin, LOW);
+      digitalWrite(LED_PIN, LOW);
       delay(500);
     }
 
-    int waitTime = random(1, 5);
-    Serial.println(waitTime);
+    // Wait N seconds BEFORE responding
+    delay(n * 1000);
+
+    // Respond with N+1 (this fixes “48 -> 49”)
+    Serial.println(n + 1);
   }
 }
