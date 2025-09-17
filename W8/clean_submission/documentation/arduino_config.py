a8d0e3dc0b19c7cf6080a8bec3872a0c47009924
Lethe <barbarareincalma@gmail.com>
2025-09-18 07:37:16 +1000

    Update arduino_config.py

--- W8/clean_submission/documentation/arduino_config.py
+++ W8/clean_submission/documentation/arduino_config.py
@@ -5,15 +5,15 @@ Replace these values with your actual Arduino IoT Cloud credentials
 
 # Arduino IoT Cloud Credentials
 # Get these from: https://create.arduino.cc/iot/
-ARDUINO_CLIENT_ID = "jkY0G7r38gpcbzQojvQN7ufpe5FIJCzA"  # You'll need to get this from Arduino IoT Cloud
-ARDUINO_CLIENT_SECRET = "dHt2wsy1YDk1gE5KmFtDmyzi9jgmugicUt3zjphmergzvO0lWD8tkzuE4tJulsB4"  # You'll need to get this from Arduino IoT Cloud
-ARDUINO_DEVICE_ID = "157c1247-9b93-4244-9783-b4d033ad401a"  # From your Arduino code
-ARDUINO_THING_ID = "6e6c3bdf-e3c6-48ba-badc-82c378d3ca23"  # You'll need to get this from Arduino IoT Cloud
+ARDUINO_CLIENT_ID = ""  # You'll need to get this from Arduino IoT Cloud
+ARDUINO_CLIENT_SECRET = ""  # You'll need to get this from Arduino IoT Cloud
+ARDUINO_DEVICE_ID = ""  # From your Arduino code
+ARDUINO_THING_ID = ""  # You'll need to get this from Arduino IoT Cloud
 
 # Network credentials (from your Arduino setup)
-WIFI_SSID = "WiFi-6C16"
-WIFI_PASSWORD = "09249851"
-DEVICE_KEY = "M!fFYXQKwAtXjT3H0c0eI0gQa"
+WIFI_SSID = "you wifi"
+WIFI_PASSWORD = "your wifi password"
+DEVICE_KEY = "your key"
 
 # Variable names in your Arduino IoT Cloud thing
 # These should match the variable names you created in your thing
@@ -21,11 +21,3 @@ ACCEL_X_VARIABLE = "accelerometer_x"
 ACCEL_Y_VARIABLE = "accelerometer_y" 
 ACCEL_Z_VARIABLE = "accelerometer_z"
 
-# Alternative: Use environment variables (more secure)
-import os
-
-# Uncomment these lines if you want to use environment variables instead:
-# ARDUINO_CLIENT_ID = os.getenv("ARDUINO_CLIENT_ID", "your_client_id_here")
-# ARDUINO_CLIENT_SECRET = os.getenv("ARDUINO_CLIENT_SECRET", "your_client_secret_here")
-# ARDUINO_DEVICE_ID = os.getenv("ARDUINO_DEVICE_ID", "your_device_id_here")
-# ARDUINO_THING_ID = os.getenv("ARDUINO_THING_ID", "your_thing_id_here")
